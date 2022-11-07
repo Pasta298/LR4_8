7435cf0ad80d8230e075b2f0e0216f7664c87d94
Pasta298 <volodiapas@gmail.com>
2022-11-07 10:50:11 +0200

    Змінено файл FileUtils.java

--- src/utilities/FileUtils.java
+++ src/utilities/FileUtils.java
@@ -6,10 +6,8 @@ import java.io.*;
 
 public class FileUtils {
     public static void saveAviacompany(Aviacompany aviacompany, String filename) {
-        try (FileOutputStream fileOutputStream = new FileOutputStream(new File(filename + ".txt"))) {
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(new File(filename + ".txt")))) {
             objectOutputStream.writeObject(aviacompany);
-            objectOutputStream.close();
             System.out.println("Aviacompany saved successfully!");
         } catch (IOException e) {
             System.out.println("Error while trying to save!");
@@ -17,10 +15,8 @@ public class FileUtils {
     }
     public static Aviacompany loadAviacompany(String filename) {
         Aviacompany aviacompany = new Aviacompany();
-        try (FileInputStream fileInputStream = new FileInputStream(new File(filename + ".txt"))) {
-            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(new File(filename + ".txt")))) {
             aviacompany = (Aviacompany) objectInputStream.readObject();
-            objectInputStream.close();
             System.out.println("Aviacompany loaded successfully!");
         } catch (Exception e) {
             System.out.println("Error while trying to load!");
